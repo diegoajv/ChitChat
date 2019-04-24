06f3b844e7d9d73f7f742e534818535d682aed22
diegoajv <diegojosevilla@outlook.com>
2019-04-24 06:11:44 -0500

    Feat[router]: Add route to serve public files

--- router/router.go
+++ router/router.go
@@ -6,10 +6,15 @@ import (
 	"github.com/diegoajv/ChitChat/controllers"
 )
 
-// GetRouter creates a multiplexer
+// GetRouter returns app routes
 func GetRouter() *http.ServeMux {
 	router := http.NewServeMux()
 
+	// Configure static files
+	files := http.FileServer(http.Dir("public"))
+	router.Handle("/static/", http.StripPrefix("/static/", files))
+
+	// App routes
 	router.HandleFunc("/", controllers.Index)
 	router.HandleFunc("/login", controllers.LogIn)
 
